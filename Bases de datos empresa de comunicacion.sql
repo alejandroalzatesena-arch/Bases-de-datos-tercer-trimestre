9fe08763ac09237f601e0f1f14e7797c9215d49c
Alejandro Alzate <alejandroalzatesena@gmail.com>
2026-02-05 17:24:52 -0500

    Bases de datos empresa de comunicacion

--- Bases de datos empresa de comunicacion.sql
+++ Bases de datos empresa de comunicacion.sql
@@ -10,3 +10,27 @@ CREATE TABLE Revista (
     titulo VARCHAR(100) NOT NULL,
     tipo VARCHAR(50)
 );
+
+CREATE TABLE Empleado (
+    dni VARCHAR(20) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20),
+    codigo_sucursal INT NOT NULL
+);
+
+CREATE TABLE Periodista (
+    dni VARCHAR(20) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    especialidad VARCHAR(50),
+    codigo_sucursal INT NOT NULL
+);
+
+CREATE TABLE Numero (
+    id_numero INT AUTO_INCREMENT,
+    num_registro_revista INT,
+    num_paginas INT,
+    fecha DATE,
+    PRIMARY KEY (id_numero, num_registro_revista)
+);
+
+SHOW tables
